e39c2fddf2c592c79f6dc8f4fbe38f877c00a159
Sacha Delanoue <sacha.delanoue@gmail.com>
2013-07-25 18:08:45 +0200

    First view

--- catalog/views.py
+++ catalog/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from catalog.models import Murder
+from django.shortcuts import render_to_response
+
+def homepage(request):
+    murders = Murder.objects.all()
+    return render_to_response('homepage.html', { 'murders': murders })
