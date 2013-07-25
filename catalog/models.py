f6e18e94d0f197b28ea04a8a0cb033e4f45b30aa
Sacha Delanoue <sacha.delanoue@gmail.com>
2013-07-25 16:04:39 +0200

    Add catalog models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Murder(models.Model):
+    name = models.CharField(max_length=64)
+    author = models.CharField(max_length=128)
+    date = models.SmallIntegerField()
+    rating = models.FloatField()
+    min_players = models.SmallIntegerField()
+    max_players = models.SmallIntegerField()
+    organizers = models.SmallIntegerField()
+    action_points = models.BooleanField()
+    link = models.URLField(null=True)
+    synopsis = models.TextField()
+
+class Instance(models.Model):
+    murder = models.ForeignKey(Murder)
+    date = models.DateTimeField()
