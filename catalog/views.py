82a4253c3ce7b67b9f72ec006b1eb85084a19c9e
Sacha Delanoue <sacha.delanoue@gmail.com>
2013-07-30 17:58:24 +0200

    Add login

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,13 @@
 from catalog.models import Murder
 from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
 
 def homepage(request):
     murders = Murder.objects.order_by('name')
-    return render_to_response('homepage.html', { 'murders': murders })
+    return render_to_response('homepage.html', { 'murders': murders },
+            context_instance=RequestContext(request))
 
 def murder(request, murder_id):
     murder = get_object_or_404(Murder, pk=murder_id)
-    return render_to_response('murder.html', { 'murder': murder })
+    return render_to_response('murder.html', { 'murder': murder },
+            context_instance=RequestContext(request))
--- murders/urls.py
+++ murders/urls.py
@@ -7,5 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
         (r'^admin/', include(admin.site.urls)),
         (r'^murder', include('catalog.urls')),
+        (r'^login$', 'django.contrib.auth.views.login',
+            {'template_name': 'login.html'}),
         (r'^$', 'catalog.views.homepage'),
 )
