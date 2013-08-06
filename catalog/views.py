dc3c992baa1e8c6a47791b58714b21d628ae5552
Sacha Delanoue <sacha.delanoue@gmail.com>
2013-08-06 11:55:53 +0200

    Better home page

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,22 @@
-from catalog.models import Murder
+from catalog.models import Murder, Instance
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+import datetime
 
 def homepage(request):
     murders = Murder.objects.order_by('name')
-    return render_to_response('homepage.html', { 'murders': murders },
-            context_instance=RequestContext(request))
+    recent = Instance.objects \
+            .filter(date__lte=datetime.date.today()) \
+            .order_by('-date')[:10]
+    next = Instance.objects \
+            .filter(date__gte=datetime.date.today()) \
+            .order_by('date')[:10]
+
+    return render_to_response('homepage.html', {
+        'murders': murders,
+        'recent': recent,
+        'next': next,
+        } ,context_instance=RequestContext(request))
 
 def murder(request, murder_id):
     murder = get_object_or_404(Murder, pk=murder_id)
